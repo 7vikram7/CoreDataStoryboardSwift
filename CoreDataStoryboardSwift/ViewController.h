f629d4fd460807b643eaa3df7cfba4b60fa2a19b
Sumeet Agrawal <sumeet.agrawal@mobisoftinfotech.com>
2015-11-02 19:13:40 +0530

    Created project and added git ignore

---
+++ CoreDataStoryboardSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataStoryboardSwift
+//
+//  Created by Vikram Gore on 02/11/15.
+//  Copyright © 2015 7vikram7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataStoryboardSwift/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataStoryboardSwift
+//
+//  Created by Vikram Gore on 02/11/15.
+//  Copyright © 2015 7vikram7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
