5d038c0cb5e47dca1432bec805e51d91bb3cf1e2
Vikram Gore <sumeet.agrawal@mobisoftinfotech.com>
2015-11-03 17:51:31 +0530

    Navigation to Squads-Transfers Viewcontroler

---
+++ CoreDataStoryboardSwift/Application/Controllers/MenuScreens/SquadsTransfersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SquadsTransfersViewController.h
+//  CoreDataStoryboardSwift
+//
+//  Created by Vikram Gore on 03/11/15.
+//  Copyright © 2015 7vikram7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SquadsTransfersViewController : UIViewController
+
+@end
